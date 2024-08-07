17999db140bc48ad75bbfc9974cf79141fe7c423
moradiya235 <moradiyakishan235@gmail.com>
2024-08-07 11:48:31 +0530

    second

---
+++ src/Pages/Homepage/Home1.js
@@ -0,0 +1,16 @@
+import { Box, Container } from '@mui/material'
+import React from 'react'
+
+function Home1() {
+  return (
+    <Box sx={{}}>
+       <Container>
+        <Box>
+
+        </Box>
+       </Container> 
+    </Box>
+  )
+}
+
+export default Home1
---
+++ src/componnents/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home1 from '../Pages/Homepage/Home1'
+
+function Homepage() {
+  return (
+    <div>
+        <Home1/>
+    </div>
+  )
+}
+
+export default Homepage
